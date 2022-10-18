0e39237e9f172eb0f3cbffa3806923cca724312b
Calinpet <92047573+Calinpet@users.noreply.github.com>
2022-10-18 12:06:45 -0400

    created index page and rendering watche

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
-  
+  # route for watches index
+  path('watches/', views.watches_index, name='index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -8,3 +8,21 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+# Add new view
+def watches_index(request):
+  return render(request, 'watches/index.html', { 'watches': watches })  
+
+class Watch:
+  def __init__(self, make, model, movement, description, price):
+    self.make = make
+    self.model = model
+    self.mechanism = movement
+    self.description = description
+    self.price = price
+
+watches = [
+  Watch('TAG Heuer', 'Calibre 16', 'Automatic Chronograph', 'Great watch', 5400),
+  Watch('TAG Heur', 'Formula 1', 'Quartz Chronograf', 'Silver grey, fixed ceramic bezel', 2450),
+  Watch('G-shock', 'Adventure', 'Quartz', 'Good reliable watch for outdoors', 150)
+]    
