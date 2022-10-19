eb29ce10588f0064634bcb843ee39ce0bf097eb0
Calinpet <92047573+Calinpet@users.noreply.github.com>
2022-10-19 01:07:18 -0400

    made and run the migration for Service model

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,12 @@ from django.db import models
 # Import the reverse function
 from django.urls import reverse
 
+# A tuple of 2-tuples
+PERIOD = (
+  ('A', 'Annualy'),
+  ('Q', 'Quartely'),
+  ('M', 'Monthly')
+)
 
 # Create your models here.
 class Watch(models.Model):
@@ -18,7 +24,20 @@ class Watch(models.Model):
 # Add this method
   def get_absolute_url(self):
      return reverse('detail', kwargs={'watch_id': self.id})
-     
+
 class Service(models.Model):
   date = models.DateField()
-  period = models.CharField(max_length=1)  
+  period = models.CharField(
+    max_length=1,
+    # add the 'choices' field option
+    choices=PERIOD,
+    # set the default value for meal to be 'B'
+    default=PERIOD[0][0]
+  ) 
+
+  # Create a watch_id FK
+  cat = models.ForeignKey(Watch, on_delete=models.CASCADE)
+
+  def __str__(self):
+    # Nice method for obtaining the friendly value of a Field.choice
+    return f"{self.get_period_display()} on {self.date}"
